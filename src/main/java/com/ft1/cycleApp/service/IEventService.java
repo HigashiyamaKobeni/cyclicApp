4219ecadbc42462b8bf362261a90cc6cd3a537bb
山东小红 <79633573+HigashiyamaKobeni@users.noreply.github.com>
2023-07-19 16:32:44 +0800

    Update IEventService.java

--- src/main/java/com/ft1/cycleApp/service/IEventService.java
+++ src/main/java/com/ft1/cycleApp/service/IEventService.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public interface IEventService {
-    void insertEvent(Event newEvent);
+    void insertEvent(Event newEvent, Integer cycleId);
 
     void deleteEvent(Integer eventId);
 
