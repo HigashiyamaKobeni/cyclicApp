41730b501961645a65efd80b20f90ac499962ee2
山东小红 <79633573+HigashiyamaKobeni@users.noreply.github.com>
2023-07-19 16:53:29 +0800

    Update EventMapper.java

--- src/main/java/com/ft1/cycleApp/mapper/EventMapper.java
+++ src/main/java/com/ft1/cycleApp/mapper/EventMapper.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public interface EventMapper {
-    Integer insertEvent(Event newEvent, Integer cycleId);
+    Integer insertEvent(Event newEvent, Integer userId);
 
     Integer deleteEvent(Integer eventId);
 
